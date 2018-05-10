e648a4caf64189dd4dca23b31513204bc14cc6e4
Chito CHENG <chito.cheng@mirumagency.com>
2018-05-10 17:35:39 +0800

    image updated

--- globe/globe.js
+++ globe/globe.js
@@ -105,7 +105,7 @@ DAT.Globe = function(container, opts) {
     shader = Shaders['earth'];
     uniforms = THREE.UniformsUtils.clone(shader.uniforms);
 
-    uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir+'world.jpg');
+    uniforms['texture'].value = THREE.ImageUtils.loadTexture(imgDir+'world_bak_01.jpg');
 
     material = new THREE.ShaderMaterial({
 
